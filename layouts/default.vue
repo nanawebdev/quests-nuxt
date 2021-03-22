c3662c7bc179f1e28bea26042728054ce2d1fadf
nanawebdev <nanaprettysinger@gmail.com>
2021-03-22 12:14:52 +0300

    Вернула навигационную панель на место)

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,16 +15,16 @@
 export default {
   computed: {
     pageClass() {
-      if (this.$route.path === '/profile') {
-        return 'profile-page'
-      }
       if (this.$route.path === '/news') {
         return 'news-list-page'
+      }
+      if (this.$route.path === '/profile') {
+        return 'profile-page'
       }
       return ''
     },
     canShowNavigationPanel() {
-      if (['/', '/news', '/profile'].includes(this.$route.path)) {
+      if (['/quests', '/news', '/profile'].includes(this.$route.path)) {
         return true
       }
       return false
