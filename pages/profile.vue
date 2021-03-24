85941a52838dd078bc3b65492fa6cf8a73a458f8
nanawebdev <nanaprettysinger@gmail.com>
2021-03-24 19:45:40 +0300

    Меняет размер кнопке Плюс

--- pages/profile.vue
+++ pages/profile.vue
@@ -44,22 +44,24 @@
           Мои квесты
         </div>
         <div>
-          <svg
-            class="icon-plus"
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M8 1V15M1 8H15"
-              stroke="white"
-              stroke-opacity="0.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
+          <button class="button-add">
+            <svg
+              class="icon-plus"
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8 1V15M1 8H15"
+                stroke="white"
+                stroke-opacity="0.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
         </div>
       </div>
 
