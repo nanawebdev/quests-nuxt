168ebeab14226f240bb4602fcebe0db26f94805b
nanawebdev <nanaprettysinger@gmail.com>
2021-03-24 12:01:41 +0300

    добавила ограничения в заголовки\тексты превью
    по высоте чтоб максимум 2 строчки отображалось

--- components/QuestPreview.vue
+++ components/QuestPreview.vue
@@ -14,9 +14,23 @@
         </div>
       </div>
       <div class="preview-text">
-        <h3 class="preview-title">
-          Ветер перемен
-        </h3>
+        <div class="button-title-wrapper">
+          <h3 class="preview-title">
+            Ветер перемен Ветер перемен Ветер перемен Ветер перемен Ветер перемен Ветер перемен Ветер перемен Ветер перемен Ветер перемен Ветер перемен
+          </h3>
+          <button v-if="hasOpenBottomsheetButton" type="button" class="open-bottomsheet js-open-bottomsheet">
+            <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill="#fff" fill-opacity="0" d="M0 0h24v24H0z" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M19 14a2 2 0 110-4 2 2 0 010 4zm-7 0a2 2 0 110-4 2 2 0 010 4zm-7 0a2 2 0 110-4 2 2 0 010 4z"
+                fill="#fff"
+                fill-opacity=".5"
+              />
+            </svg>
+          </button>
+        </div>
         <div>
           <div class="episodes-wrapper">
             <svg class="episodes-icon" width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -34,23 +48,14 @@
         </div>
         <p class="quest-description">
           Что будет если каждый из нас возьмёт пистолет и выстрелит
-          вверх?
+          вверх? Что будет если каждый из нас возьмёт пистолет и выстрелит
+          вверх? Что будет если каждый из нас возьмёт пистолет и выстрелит
+          вверх? Что будет если каждый из нас возьмёт пистолет и выстрелит
+          вверх? Что будет если каждый из нас возьмёт пистолет и выстрелит
+          вверх?324234
         </p>
         <div class="divider"></div>
       </div>
-
-      <button v-if="hasOpenBottomsheetButton" type="button" class="open-bottomsheet js-open-bottomsheet">
-        <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill="#fff" fill-opacity="0" d="M0 0h24v24H0z" />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M19 14a2 2 0 110-4 2 2 0 010 4zm-7 0a2 2 0 110-4 2 2 0 010 4zm-7 0a2 2 0 110-4 2 2 0 010 4z"
-            fill="#fff"
-            fill-opacity=".5"
-          />
-        </svg>
-      </button>
     </component>
   </li>
 </template>
